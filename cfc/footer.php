d48405b8011613aa6394258afa632bc5ba053976
Andrew Fox <andrew@andrewfox.co.uk>
2013-02-04 11:50:24 +0000

    .

--- cfc/footer.php
+++ cfc/footer.php
@@ -12,6 +12,13 @@
 ?>
 
 
+		<ul id="footer-images">
+			<li><img src="<?php bloginfo( 'template_directory' ); ?>/img/footer-1.jpg" alt="Photo: meeting"/></li>
+			<li><img src="<?php bloginfo( 'template_directory' ); ?>/img/footer-2.jpg" alt="Photo: architectural detail"/></li>
+			<li><img src="<?php bloginfo( 'template_directory' ); ?>/img/footer-3.jpg" alt="Photo: architectural detail"/></li>
+			<li><img src="<?php bloginfo( 'template_directory' ); ?>/img/footer-4.jpg" alt="Photo: notepad"/></li>
+		</ul>
+
 
 		<footer role="contentinfo">
 
