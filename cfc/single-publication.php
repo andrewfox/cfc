d2274570fd19e4f7d9a6afdd9e4464e742cb8c8f
Andrew Fox <andrew@andrewfox.co.uk>
2013-02-05 17:59:43 +0000

    .

--- cfc/single-publication.php
+++ cfc/single-publication.php
@@ -3,7 +3,7 @@
  * The Template for displaying all single blog posts.
  *
  * @package WordPress
- * @subpackage Boilerplate
+ * @subpackage CFC
  * @since Boilerplate 1.0
  */
 
@@ -13,15 +13,23 @@ get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 
+
 		<article id="single-<?php the_ID(); ?>" class="main clearfix">
 
 
 			<p class="page-title"><a href="<?php echo get_page_link(16); ?>">Publications</a></p>
 			<h1 class="page-title"><?php the_title(); ?></h1>
 
-				<?php if ( has_post_thumbnail() ) { ?>
-					<a href="<?php the_field('publication-file'); ?>" class="publication-image"><?php the_post_thumbnail('medium'); ?></a>
-				<?php } ?>
+			<?php if ( has_post_thumbnail() ) { ?>
+
+			<a href="<?php the_field('publication-file'); ?>" class="publication-image">
+				<?php the_post_thumbnail('medium'); ?>
+				<span>Download &lsquo;<?php the_title(); ?>&rsquo;</span>
+			</a>
+
+			<?php } ?>
+
+
 
 			<div class="entry-content">
 
@@ -34,6 +42,8 @@ get_header(); ?>
 				<p class="download"><a href="<?php the_field('publication-file'); ?>">Download &lsquo;<?php the_title(); ?>&rsquo;</a></p>
 
 			</div> <!-- /.entry-content -->
+
+
 
 
 		</article>
