dac7264786209089cb625b96bc92ef448e5fe30e
Andrew Fox <andrew@andrewfox.co.uk>
2013-02-06 16:38:34 +0000

    .

--- cfc/js/main.js
+++ cfc/js/main.js
@@ -3,7 +3,18 @@ $(document).ready(function() {
 
 /* Slideshow */
 
-$('.slideshow li').addClass( 'off' ).filter(':lt(4)').removeClass( 'off' ); 
+$('.slideshow li').addClass( 'off' ).filter(':lt(4)').removeClass( 'off' );
+
+$(".arrow.forward").click(function() {
+	$('.slideshow li').addClass( 'off' ).filter(':gt(4)').removeClass( 'off' );
+	$('.arrow').removeClass( 'off' );
+	$('.arrow.back').addClass( 'off' );
+});
+$(".arrow.back").click(function() {
+	$('.slideshow li').addClass( 'off' ).filter(':lt(4)').removeClass( 'off' );
+	$('.arrow').removeClass( 'off' );
+	$('.arrow.back').addClass( 'off' );
+});
 
 
 });
