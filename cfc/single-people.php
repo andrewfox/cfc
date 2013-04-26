d05d1950295e4df455ab90322b4510f0f6f6321a
Andrew Fox <andrew@andrewfox.co.uk>
2013-04-26 16:02:40 +0100

    People-details improvements

--- cfc/single-people.php
+++ cfc/single-people.php
@@ -35,82 +35,84 @@ get_header(); ?>
 			</div> <!-- /.people-content -->
 
 
-
-			<ul class="people-details-nav">
-				<?php if (get_field('people-directories_information')): ?><li><a href="#people-directories_information">Directories Information</a></li><?php endif; ?>
-				<?php if (get_field('people-area_of_practice')): ?><li><a href="#people-area_of_practice">Areas of Practice</a></li><?php endif; ?>
-				<li><a href="#people-case_news"><?php the_title(); ?> Case News &amp;&nbsp;Press</a></li>
-			</ul>
-
-
-
 			<div class="people-details">
 
 
-				<?php if (get_field('people-directories_information')): ?>
-				<div id="people-directories_information" class="people-tab">
-					<h2 class="tab-title">Directories Information</h2>
-					<?php the_field('people-directories_information'); ?>
-				</div>
-				<?php endif; ?>
 
-
-				<?php if (get_field('people-area_of_practice')): ?>
-				<div id="people-area_of_practice" class="people-tab">
-					<h2 class="tab-title">Areas of Practice</h2>
-					<?php the_field('people-area_of_practice'); ?>
-				</div>
-				<?php endif; ?>
-
-
-				<div id="people-case_news" class="people-tab">
-					<h2 class="tab-title"><?php the_title(); ?> Case News &amp;&nbsp;Press</h2>
-					<?php
-						$news = get_posts(array(
-							'post_type' => 'post',
-							'meta_query' => array(
-								array(
-									'key' => 'post-people', // name of custom field
-									'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
-									'compare' => 'LIKE'
-								)
-							)
-						));
-					?>
-					<?php if( $news ): ?>
-						<ul>
-						<?php foreach( $news as $new ): ?>
-							<li>
-							<?php if ( get_field('post-url', $new->ID) ) { // external link ?>
-			
-								<a href="<?php the_field('post-url',$new->ID); ?>" rel="bookmark">
-									<span class="post-title"><?php echo get_the_title( $new->ID ); ?>&nbsp;<span class="ss">redirect</span></span>
-								</a>
-			
-							<?php } elseif ( in_category('4', $new->ID) ) { // short ?> 
-							
-								<?php echo get_the_title( $new->ID ); ?>
-			
-							<?php } else { // regular ?> 
-			
-								<a href="<?php echo get_permalink( $new->ID ); ?>" rel="bookmark">
-									<?php echo get_the_title( $new->ID ); ?>
-								</a>
-			
-							<?php } ?> 
-							</li>
-						<?php endforeach; ?>
+				<ul class="people-details-nav">
+					<?php if (get_field('people-directories_information')): ?><li><a href="#people-directories_information">Directories Information</a></li><?php endif; ?>
+					<?php if (get_field('people-area_of_practice')): ?><li><a href="#people-area_of_practice">Areas of Practice</a></li><?php endif; ?>
+					<li><a href="#people-case_news"><?php the_title(); ?> Case News &amp;&nbsp;Press</a></li>
+				</ul>
 
 
 
+				<div class="people-details-sections">
 
 
-
-
-
-						</ul>
+					<?php if (get_field('people-directories_information')): ?>
+					<div id="people-directories_information" class="people-tab">
+						<h2 class="tab-title">Directories Information</h2>
+						<?php the_field('people-directories_information'); ?>
+					</div>
 					<?php endif; ?>
-				</div>
+
+
+					<?php if (get_field('people-area_of_practice')): ?>
+					<div id="people-area_of_practice" class="people-tab">
+						<h2 class="tab-title">Areas of Practice</h2>
+						<?php the_field('people-area_of_practice'); ?>
+					</div>
+					<?php endif; ?>
+
+
+					<div id="people-case_news" class="people-tab">
+						<h2 class="tab-title"><?php the_title(); ?> Case News &amp;&nbsp;Press</h2>
+						<?php
+							$news = get_posts(array(
+								'post_type' => 'post',
+								'meta_query' => array(
+									array(
+										'key' => 'post-people', // name of custom field
+										'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+										'compare' => 'LIKE'
+									)
+								)
+							));
+						?>
+						<?php if( $news ): ?>
+							<ul>
+							<?php foreach( $news as $new ): ?>
+								<li>
+								<?php if ( get_field('post-url', $new->ID) ) { // external link ?>
+				
+									<a href="<?php the_field('post-url',$new->ID); ?>" rel="bookmark">
+										<span class="post-title"><?php echo get_the_title( $new->ID ); ?>&nbsp;<span class="ss">redirect</span></span>
+									</a>
+				
+								<?php } elseif ( in_category('4', $new->ID) ) { // short ?> 
+								
+									<?php echo get_the_title( $new->ID ); ?>
+				
+								<?php } else { // regular ?> 
+				
+									<a href="<?php echo get_permalink( $new->ID ); ?>" rel="bookmark">
+										<?php echo get_the_title( $new->ID ); ?>
+									</a>
+				
+								<?php } ?> 
+								</li>
+							<?php endforeach; ?>
+
+
+							</ul>
+						<?php endif; ?>
+					</div>
+
+
+
+				</div> <!-- /.people-details-sections -->
+
 
 
 			</div> <!-- /.people-details -->
